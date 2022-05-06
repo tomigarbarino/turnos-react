d0970fd03168dc8d9b58d0e914edbeb1c77e23fd
tomigarbarino <82911909+tomigarbarino@users.noreply.github.com>
2022-05-05 22:48:11 -0300

    Update ListadoPacientes.jsx

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -9,7 +9,7 @@ const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
                     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
                     <p className="text-xl mt-5 mb-10 text-center">
                         Administra tus {''}
-                        <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
+                        <span className="text-indigo-600 font-bold ">Pacientes y Turnos</span>
                     </p>
 
                     { pacientes.map( paciente => (
